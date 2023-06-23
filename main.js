641b619cafb0bbd3a4e203e5daadc18d2831deb6
WescAlves <wesley.c.alves98@gmail.com>
2023-06-23 13:54:38 -0300

    Criando e atribuindo ações ao botão de logout

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const MatBtn = document.querySelector("#sign-in-btn");
 const LoginBtn = document.querySelector("#log-in-btn");
-const divBtn = document.querySelector("botoes");
+const divBtn = document.querySelector(".botoes");
 class usuario  {
     constructor (user, password, log){
         this.user = user;
@@ -12,7 +12,7 @@ let userAdmin = new usuario("admin", "admin", false);
 let Wesley = new usuario("wesley", "wesley", true);
 localStorage.setItem(userAdmin.user, JSON.stringify(userAdmin));
 localStorage.setItem(Wesley.user, JSON.stringify(Wesley));
-console.log(localStorage);
+//console.log(localStorage);
 
 const pegaItem = function(key){
     return JSON.parse(localStorage.getItem(key));
@@ -33,12 +33,28 @@ window.addEventListener("load", function(){
         let resposta = logado(element);
         if(resposta===1){
             let loggedUser = pegaItem(element);
+            //console.log(loggedUser);
             MatBtn.remove();
             LoginBtn.remove();
-            console.log(loggedUser); 
+            let p = document.createElement("p");
+            p.textContent = `Bem vindo ${loggedUser.user}`
+            p.id = 'welcome';
+            divBtn.appendChild(p);
+            let logoutBtn = document.createElement("button");
+            logoutBtn.textContent = 'Sair da conta';
+            divBtn.appendChild(logoutBtn);
+            logoutBtn.addEventListener("click", function(){
+                loggedUser.log = false;
+                localStorage.setItem(loggedUser.user, JSON.stringify(loggedUser));
+                console.log(loggedUser);
+                p.remove();
+                logoutBtn.remove();
+                divBtn.innerHTML = '<button class="btn" id="sign-in-btn"><strong>Matricule-se</strong></button> <button class="btn" id="log-in-btn"><strong>Login</strong></button>'
+            });
+             
         }     
         else{
-            console.log('Não logado')
+            console.log('Não logado');
         }   
     });    
     });
