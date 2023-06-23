a8fa9368703c25ffc0fefe8a8b6ae9c361080c13
WescAlves <wesley.c.alves98@gmail.com>
2023-06-23 18:15:18 -0300

    Modal para matricular funcionando

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const MatBtn = document.querySelector("#sign-in-btn");
 const LoginBtn = document.querySelector("#log-in-btn");
 const divBtn = document.querySelector(".botoes");
@@ -9,10 +10,15 @@ class usuario  {
     }
 }
 let userAdmin = new usuario("admin", "admin", false);
-let Wesley = new usuario("wesley", "wesley", true);
+let Wesley = new usuario("wesley", "wesley", false);
 localStorage.setItem(userAdmin.user, JSON.stringify(userAdmin));
 localStorage.setItem(Wesley.user, JSON.stringify(Wesley));
 //console.log(localStorage);
+
+const adicionaUser = function(user, password){
+    let NovoUsuario = new usuario(user, password, false);
+    localStorage.setItem(NovoUsuario.user, JSON.stringify(NovoUsuario));
+}
 
 const pegaItem = function(key){
     return JSON.parse(localStorage.getItem(key));
@@ -50,13 +56,47 @@ window.addEventListener("load", function(){
                 p.remove();
                 logoutBtn.remove();
                 divBtn.innerHTML = '<button class="btn" id="sign-in-btn"><strong>Matricule-se</strong></button> <button class="btn" id="log-in-btn"><strong>Login</strong></button>'
-            });
-             
+            });    
         }     
         else{
             console.log('Não logado');
         }   
-    });    
+    });
+    MatBtn.addEventListener("click", function(){
+        console.log("Entrou no Matricule-se!!")
+        let modal = document.createElement("dialog");
+        console.log(modal);
+        body.appendChild(modal);
+        let MatUsr = document.createElement('input');
+        let MatPas = document.createElement('input');
+        let MatriculaBtn = document.createElement("button");
+        MatriculaBtn.style.width = '100px';
+        MatriculaBtn.textContent = "Matricular";
+        MatriculaBtn.style.marginLeft = '250px';
+        MatriculaBtn.style.marginTop = '20px';
+        MatPas.style.marginLeft = '10px'
+        MatUsr.placeholder = "Usuário";
+        MatPas.placeholder = "Senha";
+        MatUsr.style.marginTop = '25px'
+        MatUsr.style.width = '285px';
+        MatPas.style.width = '285px';
+        modal.style.width = '600px';
+        modal.style.height = '120px';
+        modal.appendChild(MatUsr);
+        modal.appendChild(MatPas);
+        modal.appendChild(MatriculaBtn);
+        modal.showModal();
+        ;
+        MatriculaBtn.addEventListener('click', function(){
+            let novoUsername = MatUsr.value;
+            let novoPassword = MatPas.value
+            adicionaUser(novoUsername, novoPassword);
+            modal.close();
+            console.log(localStorage);
+        })
+    })
+    
+
     });
 
 
